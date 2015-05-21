46c7f1712f59d2243157e4e23599ce51e4427b6d
Mac Fisher <macfisher01@gmail.com>
2015-05-21 12:08:03 -0600

    added reset function to app.js

--- app.js
+++ app.js
@@ -2,7 +2,43 @@
 
 var scheduleApp = angular.module('scheduleApp', ['firebase']);
 
-scheduleApp.controller('mainController', ['$scope', function($scope) {
+scheduleApp.controller('mainController', ['$scope', function($scope, $firebase) {
 	
-	// controller code here...
+	// connect to firebase
+	var ref = new Firebase("https://fiery-torch-6995.firebaseIO.com/days");
+	var fb = $firebase(ref);
+	
+	// function to set default data
+	$scope.reset = function() {
+		
+		fb.$set({
+			monday: {
+				name: 'Monday',
+				slots: {
+					0900: {
+						time: '9:00am',
+						booked: false
+					},
+					01100: {
+						time: '11:00am',
+						booked: false
+					}
+				}
+			},
+			tuesday: {
+				name: 'Tuesday',
+				slots: {
+					0900: {
+						time: '9:00am',
+						booked: false
+					},
+					01100: {
+						time: '11:00am',
+						booked: false
+					}
+				}
+			}
+		});
+	};
+	
 }]);
